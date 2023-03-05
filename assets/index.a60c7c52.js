4281d7270a95b32c6895260b8bd2af4687f00f9d
guihongwang <marisa@marisa.ml>
2023-03-05 22:08:51 +0000

    patchyvideo has been deploy Ze☆

---
+++ assets/index.a60c7c52.js
@@ -0,0 +1,2 @@
+import{r as f,s as t}from"./LayoutDefault.vue_vue_type_script_setup_true_lang.cbd81f62.js";/*! PatchyVideo/Platinum by VoileLabs */function g(r,a,e){f(2,arguments);var i=t(r,e),s=t(a,e);return i.getTime()===s.getTime()}export{g as i};
+//# sourceMappingURL=index.a60c7c52.js.map
---
+++ assets/index.bc823dc5.js
@@ -0,0 +1,2 @@
+import{av as e}from"./index.0790df33.js";/*! PatchyVideo/Platinum by VoileLabs */const t=()=>e("theme_dark_cover_filter",!0,{writeDefaults:!1,listenToStorageChanges:!0});export{t as u};
+//# sourceMappingURL=index.bc823dc5.js.map
