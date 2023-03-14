c9661e942371468e0758314644067649fa0e3d83
guihongwang <marisa@marisa.ml>
2023-03-14 22:08:04 +0000

    patchyvideo has been endeploy Ze☆

--- assets/CoverPlaceholder.c3f9b5b1.js
+++
@@ -1,2 +0,0 @@
-import{a1 as e,o as r,g as c}from"./index.346deee2.js";/*! PatchyVideo/Platinum by VoileLabs */const o={},a={class:"aspect-ratio-16/10 bg-gray-300 dark:bg-gray-600"};function s(t,n){return r(),c("div",a)}const d=e(o,[["render",s]]);export{d as C};
-//# sourceMappingURL=CoverPlaceholder.c3f9b5b1.js.map
--- assets/index.ce325a95.js
+++
@@ -1,2 +0,0 @@
-import{av as e}from"./index.346deee2.js";/*! PatchyVideo/Platinum by VoileLabs */const t=()=>e("theme_dark_cover_filter",!0,{writeDefaults:!1,listenToStorageChanges:!0});export{t as u};
-//# sourceMappingURL=index.ce325a95.js.map
