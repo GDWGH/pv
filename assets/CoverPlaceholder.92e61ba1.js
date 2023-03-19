3998c633a6109316360a256803d0b2bec8d6ba20
guihongwang <marisa@marisa.ml>
2023-03-19 22:08:30 +0000

    patchyvideo has been deploy Ze☆

---
+++ assets/CoverPlaceholder.92e61ba1.js
@@ -0,0 +1,2 @@
+import{a1 as e,o as r,g as c}from"./index.9c2b354e.js";/*! PatchyVideo/Platinum by VoileLabs */const o={},a={class:"aspect-ratio-16/10 bg-gray-300 dark:bg-gray-600"};function s(t,n){return r(),c("div",a)}const d=e(o,[["render",s]]);export{d as C};
+//# sourceMappingURL=CoverPlaceholder.92e61ba1.js.map
