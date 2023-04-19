825c411184ceb1c3308f5fc5a1700f2f1294e30c
guihongwang <marisa@marisa.ml>
2023-04-19 22:08:16 +0000

    patchyvideo has been deploy Ze☆

---
+++ assets/index.3b80fd99.js
@@ -0,0 +1,2 @@
+import{av as e}from"./index.b3009813.js";/*! PatchyVideo/Platinum by VoileLabs */const t=()=>e("theme_dark_cover_filter",!0,{writeDefaults:!1,listenToStorageChanges:!0});export{t as u};
+//# sourceMappingURL=index.3b80fd99.js.map
