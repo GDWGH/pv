ea2dab6228fb78a8c0866482d2ea5822b9bf8ad0
guihongwang <marisa@marisa.ml>
2023-10-16 22:08:46 +0000

    patchyvideo has been endeploy Ze☆

--- assets/index.71254344.js
+++
@@ -1,2 +0,0 @@
-import{av as e}from"./index.e20695e6.js";/*! PatchyVideo/Platinum by VoileLabs */const t=()=>e("theme_dark_cover_filter",!0,{writeDefaults:!1,listenToStorageChanges:!0});export{t as u};
-//# sourceMappingURL=index.71254344.js.map
