63e72b71597ce741bb089c82f9dc595d5b142148
guihongwang <marisa@marisa.ml>
2023-12-27 22:08:10 +0000

    patchyvideo has been deploy Ze☆

---
+++ assets/backTop.a4b6f707.js
@@ -0,0 +1,2 @@
+/*! PatchyVideo/Platinum by VoileLabs */function n(){const o=document.body.scrollTop||document.documentElement.scrollTop;o>0&&(window.requestAnimationFrame(n),window.scrollTo(0,o-o/5))}export{n as b};
+//# sourceMappingURL=backTop.a4b6f707.js.map
